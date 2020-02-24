2d662641e474a1cd70ad7b7c4b617bf5ddbeb24a
YanaEgorova <yanaegorova69.3@gmail.com>
2020-02-25 00:55:12 +0200

    Updates

---
+++ precache-manifest.41b670d770dbf5b7397a828e74034043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a43ec79dfac74ac3476f5451fbd001e",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "5cb73338cb4eae2ab9e9",
+    "url": "/goit-react-hw-02-feedback/static/css/main.fa368a6a.chunk.css"
+  },
+  {
+    "revision": "b6d83b92f418f53f1398",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ddaffa8f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-feedback/static/js/2.ddaffa8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb73338cb4eae2ab9e9",
+    "url": "/goit-react-hw-02-feedback/static/js/main.bd6cc6f6.chunk.js"
+  },
+  {
+    "revision": "287629bd9ffe432a49f4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.95026c97.js"
+  }
+]);
